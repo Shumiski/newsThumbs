ae86356aee86af82b5d1cadbb2dfce80ef2e293d
Shumiski <shumiski@MB.local>
2017-05-09 19:50:58 -0300

    image being saved 100%

--- imagesSave.php
+++ imagesSave.php
@@ -2,9 +2,15 @@
 
 $featuredArticles = $_POST["featuredArticles"];
 $latestArticles = $_POST["latestArticles"];
+$newsletterNumber = $_POST["newsletterNumber"];
 
+// Dependendo do número de articles, pega variáveis
 for ($i = 1; $i <= $featuredArticles; $i++) {
-        featuredArticle . $i;
+       $featuredArticle[$i] = $_POST["featuredArticle" . $i];
+}
+
+for ($i = 1; $i <= $latestArticles; $i++) {
+       $latestArticle[$i] = $_POST["latestArticle" . $i];
 }
 
 
@@ -26,19 +32,27 @@ $imgUrl = "https://insights.hpe.com/" . $getImgUrl;
 
 
 //Starting Intervention
-use Intervention\Image\ImageManager;
+// import the Intervention Image Manager Class
+use Intervention\Image\ImageManagerStatic as Image;
 
-function resizeImages($url){
     
-$img = Image::make(theImage($url));
+$img = Image::make("https://insights.hpe.com/content/hpe-nxt/en/articles/2017/05/thanks-to-iot-predictive-maintenance-gets-an-extreme-makeover/_jcr_content/article-image.transform/1043x496-crop/image.jpeg");
 
 // now you are able to resize the instance
-$img->resize(320, 240);
+$img->resize(null, 305, function ($constraint) {
+    $constraint->aspectRatio();
+});
+
+//crop
+$img->crop(600, 305);
+
+// paste another image
+$img->insert('images/mask.png');
 
 // finally we save the image as a new file
-$img->save('public/bar.jpg');
+$img->save('images/saved/test.jpg');
     
-}
+
 
 ?>
 
@@ -66,21 +80,14 @@ $img->save('public/bar.jpg');
 </head>
 <body>
   
-    <?php
+    <?php 
     
-
-    for ($i = 1; $i <= $featuredArticles; $i++) {
-        
-        
-        echo "<input type='text' name='featuredArticle" . $i . "'><br><br>";
-    }
-    
-
-    for ($j = 1; $j <= $latestArticles; $j++) {
-        echo "<input type='text' name='latestArticle" . $j . "'><br><br>";
-    }
+    //for ($i = 1; $i <= $featuredArticles; $i++) {
+    //    resizeImages($featuredArticle[$i], $i);    
+    //}
     
     ?>
+    
     
     
 </body>
